18583722c8416877c43d4e7e3aa9154c29265e07
admin <rizkyputri.maharani33@gmail.com>
2024-08-28 21:41:50 +0700

    [UPDATE] fix bug on API URL

--- src/main/java/com/enigma/student_report/constant/ApiUrl.java
+++ src/main/java/com/enigma/student_report/constant/ApiUrl.java
@@ -5,6 +5,6 @@ public class ApiUrl {
 
     public static final String STUDENT_URL = "/api/v1/students";
     public static final String TEACHER_URL = "/api/v1/teachers";
-    public static final String SUBJECT_URL = "/api/v1/students";
-    public static final String ENROLLMENT_URL = "/api/v1/students";
+    public static final String SUBJECT_URL = "/api/v1/subjects";
+    public static final String ENROLLMENT_URL = "/api/v1/enrollments";
 }
